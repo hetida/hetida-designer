e1ef57f42b6beaee42b74edbca2d96485ba92fe9
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-05-11 12:44:22 +0200

    remove gen_default_return_for_plot_func function

--- runtime/hetdesrun/runtime/engine/plain/execution.py
+++ runtime/hetdesrun/runtime/engine/plain/execution.py
@@ -3,15 +3,6 @@ import asyncio
 from typing import Callable, Any, Dict
 
 
-def gen_default_return_for_plot_func(outputs: dict) -> dict:
-    """Generate answer for plot component if it should not be executed
-
-    If plot component is not executed, its PlotlyJson outputs should be empty dicts.
-    This function prepares this output from the provided outputs
-    """
-    return {key: {} for key in outputs.keys()}
-
-
 async def run_func_or_coroutine(
     func_or_coro: Callable[..., Any], kwargs: Dict[str, Any]
 ) -> Dict[str, Any]:
