e18d6241c7dde95b55e9fbdc22f4469ee65a654b
pka1024 <pkasek@neusta-sd-west.de>
2022-06-02 10:14:07 +0200

    Input-Validation fix
    fix:
    - No more double error messages in short description.
    - "," is now a valid character.
    - Removed required validation in short description.

--- frontend/src/app/validation/allowed-chars-validator.ts
+++ frontend/src/app/validation/allowed-chars-validator.ts
@@ -7,7 +7,7 @@ import {
 
 export function AllowedCharsValidator(): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
-    const val = Validators.pattern(/^[\p{L}\p{N}\p{M}\p{Pc} \.\()\/=+&-]+$/u);
+    const val = Validators.pattern(/^[\p{L}\p{N}\p{M}\p{Pc} \.,\()\/=+&-]+$/u);
     return val(control) ? { allowedChars: { valid: false } } : null;
   };
 }
