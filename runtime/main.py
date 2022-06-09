af505b0df744ef350cdc29431021afd6e003d741
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-06-09 14:45:27 +0200

    use config instead of env variable

--- runtime/main.py
+++ runtime/main.py
@@ -99,7 +99,7 @@ def run_trafo_rev_deployment():
     import_transformations("./transformations", update_component_code=False, directly_into_db=True)
 
 in_memory_db = detect_in_memory_db()
-is_backend = os.environ.get("HD_IS_BACKEND_SERVICE", None)
+is_backend = runtime_config.is_backend_service
 true_equiv = [None, "", "true", "yes", "y", "ok"]
 
 if in_memory_db:
