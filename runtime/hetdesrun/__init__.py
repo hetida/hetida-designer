a8581b2e681b85e6e86fe57fe982aa7e296f447b
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-08-26 14:07:26 +0200

    improve logging context readability

--- runtime/hetdesrun/__init__.py
+++ runtime/hetdesrun/__init__.py
@@ -49,13 +49,13 @@ def configure_logging(
         "[in %(pathname)s:%(lineno)d"
         + (
             (
-                ",\n    %(currently_executed_transformation_type).1s"
-                " id: %(currently_executed_transformation_id)s"
-                ", %(currently_executed_transformation_type).1s"
-                " name: %(currently_executed_transformation_name)s"
-                ", J id: %(currently_executed_job_id)s"
-                ",\n    O id(s): %(currently_executed_operator_hierarchical_id)s"
-                ",\n    O name(s): %(currently_executed_operator_hierarchical_name)s"
+                ",\n    tr type: %(currently_executed_transformation_type)s"
+                ", tr id: %(currently_executed_transformation_id)s"
+                ", tr name: %(currently_executed_transformation_name)s"
+                ", tr tag: %(currently_executed_transformation_tag)s"
+                ", job id: %(currently_executed_job_id)s"
+                ",\n    op id(s): %(currently_executed_operator_hierarchical_id)s"
+                ",\n    op name(s): %(currently_executed_operator_hierarchical_name)s"
                 "\n]"
             )
             if log_execution_context
