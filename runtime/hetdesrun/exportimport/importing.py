d30217c2d55007e4d83c3d83faf89637a22f342e
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-09-19 15:20:09 +0200

    run format

--- runtime/hetdesrun/exportimport/importing.py
+++ runtime/hetdesrun/exportimport/importing.py
@@ -219,7 +219,7 @@ def deprecate_older_revisions_in_group(tr_json: dict) -> None:
                 logger.info(
                     "Disable transformation revision %s with released timestamp %s",
                     revision_group_tr_json["id"],
-                    revision_group_tr_json["released_timestamp"]
+                    revision_group_tr_json["released_timestamp"],
                 )
                 put_response = requests.put(
                     posix_urljoin(
