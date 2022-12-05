d76d20e46b01ff8ac453abfc7ef9ff66cab39206
pka1024 <pkasek@neusta-sd-west.de>
2022-12-05 12:18:22 +0100

    API change, component-editor-service
    feat: (api):
    - Deleted unused component-editor-service.
    - Fixed associated unit tests.

--- frontend/src/app/service/base-item/base-item-action.service.spec.ts
+++ frontend/src/app/service/base-item/base-item-action.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { MaterialModule } from 'src/app/material.module';
-import { ComponentEditorService } from '../component-editor/component-editor.service';
 import { WiringHttpService } from '../http-service/wiring-http.service';
 import { TabItemService } from '../tab-item/tab-item.service';
 import { WorkflowEditorService } from '../workflow-editor/workflow-editor.service';
@@ -14,8 +13,8 @@ describe('BaseitemActionService', () => {
     const baseItemService = jasmine.createSpy();
     const workflowService = jasmine.createSpy();
     const tabItemService = jasmine.createSpy();
-    const componentService = jasmine.createSpy();
     const wiringService = jasmine.createSpy();
+
     TestBed.configureTestingModule({
       imports: [MaterialModule],
       providers: [
@@ -30,10 +29,6 @@ describe('BaseitemActionService', () => {
         {
           provide: TabItemService,
           useValue: tabItemService
-        },
-        {
-          provide: ComponentEditorService,
-          useValue: componentService
         },
         {
           provide: WiringHttpService,
