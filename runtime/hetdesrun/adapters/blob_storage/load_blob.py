c714256c66e36a13403e9d83f15816845e1b7fd5
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-12-05 10:45:57 +0100

    fix log message meaning

--- runtime/hetdesrun/adapters/blob_storage/load_blob.py
+++ runtime/hetdesrun/adapters/blob_storage/load_blob.py
@@ -5,5 +5,5 @@ logger = logging.getLogger(__name__)
 
 
 def load_blob_from_storage(source_id: str) -> Any:
-    logger.info("Write blob to storage for source_id %s", source_id)
-    return {"test": source_id}
+    logger.info("Load blob from storage for source_id %s", source_id)
+    return {source_id}
