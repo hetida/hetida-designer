ff53c10fd65e7494a4dd5b5fba2da534ec350620
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-12-20 14:09:53 +0100

    use fastapi instead of flask

--- runtime/hetdesrun/adapters/blob_storage/authentication.py
+++ runtime/hetdesrun/adapters/blob_storage/authentication.py
@@ -1,17 +1,19 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import json
 import logging
 import urllib
+from typing import Optional
 from uuid import uuid4
 
 import boto3
 import requests
 from botocore.client import Config
-from flask import Flask, request
+from fastapi import FastAPI
 
-boto3.set_stream_logger('boto3.resources', logging.DEBUG)
+from hetdesrun.webservice.config import get_config
+from hetdesrun import VERSION
+
+boto3.set_stream_logger("boto3.resources", logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 authorize_url = "http://localhost:8080/auth/realms/minio/protocol/openid-connect/auth"
 token_url = "http://localhost:8080/auth/realms/minio/protocol/openid-connect/token"
@@ -24,78 +26,77 @@ client_id = 'account'
 client_secret = 'daaa3008-80f0-40f7-80d7-e15167531ff0'
 
 sts_client = boto3.client(
-    'sts',
-    region_name='us-east-1',
+    "sts",
+    region_name="eu-central-1",
     use_ssl=False,
-    endpoint_url='http://localhost:9000',
+    endpoint_url="http://localhost:9000",
 )
 
-app = Flask(__name__)
+app = FastAPI(title="", description="",version=VERSION, root_path=get_config().swagger_prefix)
 
 
-@app.route('/')
-def homepage():
-    text = '<a href="%s">Authenticate with keycloak</a>'
-    return text % make_authorization_url()
-
-
-def make_authorization_url():
+def make_authorization_url() -> str:
     # Generate a random string for the state parameter
     # Save it for use later to prevent xsrf attacks
 
     state = str(uuid4())
-    params = {"client_id": client_id,
-              "response_type": "code",
-              "state": state,
-              "redirect_uri": callback_uri,
-              "scope": "openid"}
+    params = {
+        "client_id": client_id,
+        "response_type": "code",
+        "state": state,
+        "redirect_uri": callback_uri,
+        "scope": "openid",
+    }
 
     url = authorize_url + "?" + urllib.parse.urlencode(params)
     return url
 
 
-@app.route('/oauth2/callback')
-def callback():
-    error = request.args.get('error', '')
-    if error:
+@app.get("/")
+def homepage() -> str:
+    text = '<a href="%s">Authenticate with keycloak</a>'
+    return text % make_authorization_url()
+
+
+@app.post("/oauth2/callback")
+def callback(error: Optional[str] = None, code: Optional[str] = None) -> str:
+    if error is not None:
+        logger.error("Error: %s", error)
         return "Error: " + error
 
-    authorization_code = request.args.get('code')
-
-    data = {'grant_type': 'authorization_code',
-            'code': authorization_code, 'redirect_uri': callback_uri}
-    id_token_response = requests.post(
-        token_url, data=data, verify=False,
-        allow_redirects=False, auth=(client_id, client_secret))
-
-    print('body: ' + id_token_response.text)
-
-    # we can now use the id_token as much as we want to access protected resources.
-    tokens = json.loads(id_token_response.text)
-    id_token = tokens['id_token']
-
-    response = sts_client.assume_role_with_web_identity(
-        RoleArn='arn:aws:iam::123456789012:user/svc-internal-api',
-        RoleSessionName='test',
-        WebIdentityToken=id_token,
-        DurationSeconds=3600
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": callback_uri,
+    }
+    access_token_response = requests.post(
+        token_url,
+        data=data,
+        verify=False,
+        allow_redirects=False,
+        auth=(client_id, client_secret),
+        timeout=90
     )
 
-    s3_resource = boto3.resource('s3',
-                                 endpoint_url='http://localhost:9000',
-                                 aws_access_key_id=response['Credentials']['AccessKeyId'],
-                                 aws_secret_access_key=response['Credentials']['SecretAccessKey'],
-                                 aws_session_token=response['Credentials']['SessionToken'],
-                                 config=Config(signature_version='s3v4'),
-                                 region_name='us-east-1')
+    # we can now use the id_token as much as we want to access protected resources.
+    tokens = json.loads(access_token_response.text)
+    access_token = tokens["asccess_token"]
 
-    bucket = s3_resource.Bucket('testbucket')
+    response = sts_client.assume_role_with_web_identity(
+        RoleArn="arn:aws:iam::123456789012:user/svc-internal-api",
+        RoleSessionName="test",
+        WebIdentityToken=access_token,
+        DurationSeconds=3600,
+    )
 
-    for obj in bucket.objects.all():
-        print(obj)
+    s3_resource = boto3.resource(
+        "s3",
+        endpoint_url="http://localhost:9000",
+        aws_access_key_id=response["Credentials"]["AccessKeyId"],
+        aws_secret_access_key=response["Credentials"]["SecretAccessKey"],
+        aws_session_token=response["Credentials"]["SessionToken"],
+        config=Config(signature_version="s3v4"),
+        region_name="eu-central-1",
+    )
 
     return "success"
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
