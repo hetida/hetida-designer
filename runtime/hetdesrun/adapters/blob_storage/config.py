dbb11c997224be14245fd538cc5764ea9ddc672f
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2022-12-21 16:55:47 +0100

    use token access manager

--- runtime/hetdesrun/adapters/blob_storage/config.py
+++ runtime/hetdesrun/adapters/blob_storage/config.py
@@ -8,16 +8,8 @@ class BlobStorageAdapterConfig(BaseSettings):
         "/mount/blob_storage_adapter_hierarchy.json",
         env="BLOB_STORAGE_HIERARCHY_LOCATION",
     )
-    client_id: str = Field("", env="BLOB_STORAGE_IDENTITY_OPENID_CLIENT_ID")
-    client_secret: str = Field("", env="BLOB_STORAGE_IDENTITY_OPENID_CLIENT_SECRET")
-    redirect_uri: str = Field("", env="BLOB_STORAGE_IDENTITY_OPENID_REDIRECT_URI")
-    authorize_url: str = Field("", env="BLOB_STORAGE_IDENTITY_AUTHORIZE_URL")
-    token_url: str = Field("", env="BLOB_STORAGE_IDENTITY_TOKEN_URL")
     endpoint_url: str = Field("", env="BLOB_STORAGE_ENDPOINT_URL")
     region_name: str = Field("eu-central-1", env="BLOB_STORAGE_REGION_NAME")
-    aws_access_key_id = Field("", env="AWS_ACCESS_KEY_ID")
-    aws_secret_access_key = Field("", env="AWS_SECRET_ACCESS_KEY")
-    aws_session_token = Field("", env="AWS_SESSION_TOKEN")
 
 
 blob_storage_adapter_config = BlobStorageAdapterConfig()
--- runtime/hetdesrun/adapters/blob_storage/service.py
+++ runtime/hetdesrun/adapters/blob_storage/service.py
@@ -1,4 +1,3 @@
-from functools import cache
 from logging import getLogger
 from typing import List
 
@@ -8,18 +7,44 @@ from mypy_boto3_s3.service_resource import S3ServiceResource
 
 from hetdesrun.adapters.blob_storage.config import get_blob_adapter_config
 from hetdesrun.adapters.blob_storage.models import BucketName, IdString
+from hetdesrun.webservice.auth_outgoing import create_or_get_named_access_token_manager
+from hetdesrun.webservice.config import get_config
 
 logger = getLogger(__name__)
 
 
-@cache
+def get_access_token() -> str:
+    service_credentials = get_config().internal_auth_client_credentials
+    assert service_credentials is not None  # for mypy
+    access_token_manager = create_or_get_named_access_token_manager(
+        "blob_adapter_auth", service_credentials
+    )
+    access_token = access_token_manager.sync_get_access_token()
+    return access_token
+
+
 def get_resource() -> S3ServiceResource:
+    sts_client = boto3.client(
+        "sts",
+        region_name=get_blob_adapter_config().region_name,
+        use_ssl=False,
+        endpoint_url=get_blob_adapter_config().endpoint_url,
+    )
+
+    response = sts_client.assume_role_with_web_identity(
+        # Amazon Resource Name (ARN)
+        RoleArn="arn:aws:iam::123456789012:user/svc-internal-api",
+        RoleSessionName="test",
+        WebIdentityToken=get_access_token(),
+        DurationSeconds=3600,
+    )
+
     s3_resource = boto3.resource(
         "s3",
         endpoint_url=get_blob_adapter_config().endpoint_url,
-        aws_access_key_id=get_blob_adapter_config().aws_access_key_id,
-        aws_secret_access_key=get_blob_adapter_config().aws_secret_access_key,
-        aws_session_token=get_blob_adapter_config().aws_session_token,
+        aws_access_key_id=response["Credentials"]["AccessKeyId"],
+        aws_secret_access_key=response["Credentials"]["SecretAccessKey"],
+        aws_session_token=response["Credentials"]["SessionToken"],
         config=Config(signature_version="s3v4"),
         region_name=get_blob_adapter_config().region_name,
     )
