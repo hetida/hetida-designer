06ea992b303bb9b26a76211296f06c9bc6c6a9da
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-01-13 19:45:09 +0100

    complete get object key strings in bucket test

--- runtime/tests/blob_storage_adapter/test_blob_storage_adapter_service.py
+++ runtime/tests/blob_storage_adapter/test_blob_storage_adapter_service.py
@@ -62,15 +62,29 @@ def test_blob_storage_service_get_s3_resource():
 
 def test_blob_storage_service_get_object_key_strings_in_bucket():
     with mock_s3():
+        client_mock = boto3.client("s3", region_name="eu-central-1")
+        client_mock.create_bucket(
+            Bucket="bucket-without-objects-name",
+            CreateBucketConfiguration={"LocationConstraint": "eu-central-1"},
+        )
+        client_mock.create_bucket(
+            Bucket="bucket-with-objects-name",
+            CreateBucketConfiguration={"LocationConstraint": "eu-central-1"},
+        
+        )
+        client_mock.put_object(Bucket="bucket-with-objects-name", Key="key")
         with mock.patch(
             "hetdesrun.adapters.blob_storage.service.get_s3_client",
-            return_value=boto3.Session(
-                aws_access_key_id="some_key_id",
-                aws_secret_access_key="some_key",
-                aws_session_token="some_token",
-                region_name="eu-central-1",
-            ).client("s3"),
+            return_value=client_mock,
         ):
             with pytest.raises(BucketNotFound) as exc_info:
-                get_object_key_strings_in_bucket("bucket_name")
-            assert "The bucket 'bucket_name' does not exist!" in str(exc_info.value)
+                get_object_key_strings_in_bucket("non_existent_bucket_name")
+            assert "The bucket 'non_existent_bucket_name' does not exist!" in str(
+                exc_info.value
+            )
+
+            empty_object_key_string_list = get_object_key_strings_in_bucket("bucket-without-objects-name")
+            assert empty_object_key_string_list == []
+
+            object_key_string_list = get_object_key_strings_in_bucket("bucket-with-objects-name")
+            assert object_key_string_list == ["key"]
