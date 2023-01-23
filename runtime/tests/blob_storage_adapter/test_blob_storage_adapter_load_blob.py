4fc6c7603666fc5dcb856fde3c9860c30320170d
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-01-23 10:29:22 +0100

    structure load blob tests

--- runtime/tests/blob_storage_adapter/test_blob_storage_adapter_load_blob.py
+++ runtime/tests/blob_storage_adapter/test_blob_storage_adapter_load_blob.py
@@ -17,7 +17,7 @@ from hetdesrun.adapters.blob_storage.models import BlobStorageStructureSource
 from hetdesrun.models.data_selection import FilteredSource
 
 
-def test_blob_storage_load_blob_from_storage(caplog):
+def test_blob_storage_load_blob_from_storage_works(caplog):
     with mock_s3():
         client_mock = boto3.client("s3", region_name="us-east-1")
         bucket_name = "i-ii"
@@ -54,6 +54,21 @@ def test_blob_storage_load_blob_from_storage(caplog):
 
                     assert struct.unpack(">i", blob) == (42,)
 
+
+def test_blob_storage_load_blob_from_storage_with_non_existing_source():
+    with mock_s3():
+        client_mock = boto3.client("s3", region_name="us-east-1")
+        bucket_name = "i-ii"
+        client_mock.create_bucket(Bucket=bucket_name)
+        client_mock.put_object(
+            Bucket=bucket_name,
+            Key="A_2022-01-02T14:23:18+00:00",
+            Body=struct.pack(">i", 42),
+        )
+        with mock.patch(
+            "hetdesrun.adapters.blob_storage.load_blob.get_s3_client",
+            return_value=client_mock,
+        ):
             with mock.patch(
                 "hetdesrun.adapters.blob_storage.load_blob.get_source_by_thing_node_id_and_metadata_key",
                 side_effect=SourceNotFound("SourceNotFound message"),
@@ -66,6 +81,21 @@ def test_blob_storage_load_blob_from_storage(caplog):
 
                 assert "SourceNotFound message" in str(exc_info.value)
 
+
+def test_blob_storage_load_blob_from_storage_with_multiple_existing_sources():
+    with mock_s3():
+        client_mock = boto3.client("s3", region_name="us-east-1")
+        bucket_name = "i-ii"
+        client_mock.create_bucket(Bucket=bucket_name)
+        client_mock.put_object(
+            Bucket=bucket_name,
+            Key="A_2022-01-02T14:23:18+00:00",
+            Body=struct.pack(">i", 42),
+        )
+        with mock.patch(
+            "hetdesrun.adapters.blob_storage.load_blob.get_s3_client",
+            return_value=client_mock,
+        ):
             with mock.patch(
                 "hetdesrun.adapters.blob_storage.load_blob.get_source_by_thing_node_id_and_metadata_key",
                 side_effect=SourceNotUnique("SourceNotUnique message"),
@@ -78,6 +108,8 @@ def test_blob_storage_load_blob_from_storage(caplog):
 
                 assert ("SourceNotUnique message") in str(exc_info.value)
 
+
+def test_blob_storage_load_blob_from_storage_with_non_existing_bucket():
     with mock_s3():
         client_mock = boto3.client("s3", region_name="us-east-1")
         with mock.patch(
@@ -101,6 +133,8 @@ def test_blob_storage_load_blob_from_storage(caplog):
                     )
                 assert "The bucket 'i-ii' does not exist!" in str(exc_info.value)
 
+
+def test_blob_storage_load_blob_from_storage_with_non_existing_object():
     with mock_s3():
         client_mock = boto3.client("s3", region_name="us-east-1")
         bucket_name = "i-ii"
@@ -131,7 +165,7 @@ def test_blob_storage_load_blob_from_storage(caplog):
 
 
 @pytest.mark.asyncio
-async def test_blob_storage_load_data():
+async def test_blob_storage_load_data_works():
     with mock.patch(
         "hetdesrun.adapters.blob_storage.load_blob.load_blob_from_storage",
         return_value=struct.pack(">i", 42),
@@ -156,10 +190,19 @@ async def test_blob_storage_load_data():
         assert "input_name" in loaded_data
         assert loaded_data["input_name"] == struct.pack(">i", 42)
 
+
+@pytest.mark.asyncio
+async def test_blob_storage_load_data_with_error():
     with mock.patch(
         "hetdesrun.adapters.blob_storage.load_blob.load_blob_from_storage",
         side_effect=ObjectNotFound("Error message"),
-    ) as mocked_load_blob_from_storage:
+    ):
+        filtered_source = FilteredSource(
+            ref_id="i-ii/A",
+            ref_id_type="SINK",
+            ref_key="A - Next Object",
+            type="Any",
+        )
         with pytest.raises(ObjectNotFound) as exc_info:
             await load_data(
                 wf_input_name_to_filtered_source_mapping_dict={
