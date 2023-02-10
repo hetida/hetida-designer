239d3280e4ae67906465619527d5e1bbf05d6a73
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-02-10 18:24:43 +0100

    iterate over correct objects

--- runtime/hetdesrun/backend/models/workflow.py
+++ runtime/hetdesrun/backend/models/workflow.py
@@ -83,7 +83,7 @@ def get_operator_and_connector_name(
                 if connector.id == connector_id:
                     assert isinstance(connector.name, str)  # hint for mypy # noqa: S101
                     connector_name = connector.name
-            for connector in operator.inputs:
+            for connector in operator.outputs:
                 if connector.id == connector_id:
                     assert isinstance(connector.name, str)  # hint for mypy # noqa: S101
                     connector_name = connector.name
