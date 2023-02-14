2569bf2b436e75447642a947362a105420ca1c0e
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-02-14 14:51:09 +0100

    end2end test store and lod object with class

--- runtime/tests/test_end_to_end_execution.py
+++ runtime/tests/test_end_to_end_execution.py
@@ -1,9 +1,18 @@
 import json
 import os
+from typing import Any
+from unittest import mock
 from uuid import uuid4
 
+import boto3
 import pytest
+from httpx import AsyncClient
+from moto import mock_s3
 
+from hetdesrun.adapters.blob_storage.models import (
+    AdapterHierarchy,
+    BlobStorageStructureSource,
+)
 from hetdesrun.models.code import CodeModule
 from hetdesrun.models.component import (
     ComponentInput,
@@ -22,14 +31,18 @@ from hetdesrun.trafoutils.io.load import load_json
 from hetdesrun.utils import get_uuid_from_seed
 
 
-async def run_workflow_with_client(workflow_json, open_async_test_client):
+async def run_workflow_with_client(
+    workflow_json: dict, open_async_test_client: AsyncClient
+) -> tuple[int, Any]:
     response = await open_async_test_client.post("engine/runtime", json=workflow_json)
     return response.status_code, response.json()
 
 
 def gen_execution_input_from_single_component(
-    component_json_path, direct_provisioning_data_dict=None, wf_wiring=None
-):
+    component_json_path: str,
+    direct_provisioning_data_dict: dict | None = None,
+    wf_wiring: WorkflowWiring | None = None,
+) -> WorkflowExecutionInput:
     """Wraps a single component into a workflow and generates the execution input json
 
     input data is provided directly
@@ -109,7 +122,9 @@ def gen_execution_input_from_single_component(
                 InputWiring(
                     workflow_input_name=comp_input.name,
                     adapter_id=1,
-                    filters={"value": direct_provisioning_data_dict[comp_input.name]},
+                    filters={"value": direct_provisioning_data_dict[comp_input.name]}
+                    if direct_provisioning_data_dict is not None
+                    else None,
                 )
                 for comp_input in comp_inputs
             ],
@@ -127,8 +142,10 @@ def gen_execution_input_from_single_component(
 
 
 async def run_single_component(
-    component_json_file_path, input_data_dict, open_async_test_client
-):
+    component_json_file_path: str,
+    input_data_dict: dict,
+    open_async_test_client: AsyncClient,
+) -> WorkflowExecutionResult:
     response = await open_async_test_client.post(
         "engine/runtime",
         json=json.loads(
@@ -143,7 +160,9 @@ async def run_single_component(
 
 
 @pytest.mark.asyncio
-async def test_null_values_pass_any_pass_through(async_test_client):
+async def test_null_values_pass_any_pass_through(
+    async_test_client: AsyncClient,
+) -> None:
     async with async_test_client as client:
         exec_result = await run_single_component(
             (
@@ -161,7 +180,9 @@ async def test_null_values_pass_any_pass_through(async_test_client):
 
 
 @pytest.mark.asyncio
-async def test_null_list_values_pass_any_pass_through(async_test_client):
+async def test_null_list_values_pass_any_pass_through(
+    async_test_client: AsyncClient,
+) -> None:
     async with async_test_client as client:
         exec_result = await run_single_component(
             (
@@ -175,7 +196,9 @@ async def test_null_list_values_pass_any_pass_through(async_test_client):
 
 
 @pytest.mark.asyncio
-async def test_null_values_pass_series_pass_through(async_test_client):
+async def test_null_values_pass_series_pass_through(
+    async_test_client: AsyncClient,
+) -> None:
     async with async_test_client as client:
         exec_result = await run_single_component(
             (
@@ -202,7 +225,9 @@ async def test_null_values_pass_series_pass_through(async_test_client):
 
 
 @pytest.mark.asyncio
-async def test_all_null_values_pass_series_pass_through(async_test_client):
+async def test_all_null_values_pass_series_pass_through(
+    async_test_client: AsyncClient,
+) -> None:
     async with async_test_client as client:
         exec_result = await run_single_component(
             (
@@ -219,7 +244,7 @@ async def test_all_null_values_pass_series_pass_through(async_test_client):
 
 
 @pytest.mark.asyncio
-async def test_nested_wf_execution(async_test_client):
+async def test_nested_wf_execution(async_test_client: AsyncClient) -> None:
     async with async_test_client as client:
         with open(
             os.path.join("tests", "data", "nested_wf_execution_input.json"),
@@ -235,3 +260,95 @@ async def test_nested_wf_execution(async_test_client):
         assert response_json["output_results_by_output_name"][
             "limit_violation_timestamp"
         ].startswith("2020-05-28T20:16:41")
+
+
+@pytest.mark.asyncio
+async def test_load_object_with_self_defined_class(
+    async_test_client: AsyncClient,
+) -> None:
+    with mock_s3():
+        client_mock = boto3.client("s3", region_name="us-east-1")
+        client_mock.create_bucket(Bucket="i-i")
+        client_mock.create_bucket(
+            Bucket="i-ii"
+        )  # needed for create_sources based on adapter structure
+        client_mock.create_bucket(
+            Bucket="i-iii"
+        )  # needed for create_sources based on adapter structure
+        client_mock.create_bucket(
+            Bucket="iii"
+        )  # needed for create_sources based on adapter structure
+        with mock.patch(
+            "hetdesrun.adapters.blob_storage.utils.get_adapter_structure",
+            return_value=AdapterHierarchy.from_file(
+                "tests/data/blob_storage/blob_storage_adapter_hierarchy.json"
+            ),
+        ), mock.patch(
+            "hetdesrun.adapters.blob_storage.structure.get_adapter_structure",
+            return_value=AdapterHierarchy.from_file(
+                "tests/data/blob_storage/blob_storage_adapter_hierarchy.json"
+            ),
+        ), mock.patch(
+            "hetdesrun.adapters.blob_storage.service.get_s3_client",
+            return_value=client_mock,
+        ), mock.patch(
+            "hetdesrun.adapters.blob_storage.load_blob.get_s3_client",
+            return_value=client_mock,
+        ), mock.patch(
+            "hetdesrun.adapters.blob_storage.write_blob.get_s3_client",
+            return_value=client_mock,
+        ):
+            async with async_test_client as client:
+                with open(
+                    os.path.join(
+                        "tests",
+                        "data",
+                        "store_object_with_class_wf_execution_input.json",
+                    ),
+                    encoding="utf8",
+                ) as f:
+                    loaded_store_object_workflow_exe_input = json.load(f)
+                (
+                    store_response_status_code,
+                    store_response_json,
+                ) = await run_workflow_with_client(
+                    loaded_store_object_workflow_exe_input, client
+                )
+
+                assert store_response_status_code == 200
+                assert store_response_json["result"] == "ok"
+
+                with open(
+                    os.path.join(
+                        "tests",
+                        "data",
+                        "load_object_with_class_wf_execution_input.json",
+                    ),
+                    encoding="utf8",
+                ) as f:
+                    loaded_load_object_workflow_exe_input = json.load(f)
+
+                parent_id = loaded_load_object_workflow_exe_input["workflow_wiring"][
+                    "input_wirings"
+                ][0]["ref_id"]
+
+                structure_response = await client.get(
+                    f"/adapters/blob/structure?parentId={parent_id}"
+                )
+                assert structure_response.status_code == 200
+
+                metadata_key = structure_response.json()["sources"][0]["metadataKey"]
+
+                loaded_load_object_workflow_exe_input["workflow_wiring"][
+                    "input_wirings"
+                ][0]["ref_key"] = metadata_key
+
+                (
+                    load_response_status_code,
+                    load_response_json,
+                ) = await run_workflow_with_client(
+                    loaded_load_object_workflow_exe_input, client
+                )
+
+                assert load_response_status_code == 200
+                assert load_response_json["result"] == "ok"
