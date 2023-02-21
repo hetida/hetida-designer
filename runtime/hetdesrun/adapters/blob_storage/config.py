15ecfa4326bafd3c03a06d1d79e15d396a99012f
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-02-21 18:12:13 +0100

    load config from env file, add descriptions

--- runtime/hetdesrun/adapters/blob_storage/config.py
+++ runtime/hetdesrun/adapters/blob_storage/config.py
@@ -1,4 +1,6 @@
-from pydantic import BaseSettings, Field
+import os
+
+from pydantic import BaseSettings, Field, validator
 
 
 class BlobStorageAdapterConfig(BaseSettings):
@@ -6,14 +8,48 @@ class BlobStorageAdapterConfig(BaseSettings):
 
     adapter_hierarchy_location: str = Field(
         "",
+        description=(
+            "Path to the adapter hierarchy json file. "
+            "Must be set to enable the BLOB storage adapter REST API. "
+        ),
         env="BLOB_STORAGE_ADAPTER_HIERARCHY_LOCATION",
+        example="/mnt/blob_storage_adapter_hierarchy.json",
     )
-    access_duration: int = Field(3600, env="BLOB_STORAGE_ACCESS_DURATION")
-    endpoint_url: str = Field("", env="BLOB_STORAGE_ENDPOINT_URL")
-    region_name: str = Field("eu-central-1", env="BLOB_STORAGE_REGION_NAME")
+    access_duration: int = Field(
+        3600,
+        description=(
+            "Sets the request parameter `DurationSeconds` "
+            "in the role session authentication towards the BLOB storage. "
+            "Must be at least 900 (15 minutes) and smaller than "
+            "the maximum session duration setting for the role, which is at most 12 hours."
+        ),
+        env="BLOB_STORAGE_ACCESS_DURATION",
+    )
+    endpoint_url: str = Field(
+        "",
+        description="URL under which the BLOB storage is accessible.",
+        env="BLOB_STORAGE_ENDPOINT_URL",
+        example="/mnt/blob_storage_adapter_hierarchy.json",
+    )
+    region_name: str = Field(
+        "eu-central-1", description="", env="BLOB_STORAGE_REGION_NAME"
+    )
+
+    @validator("access_duration")
+    def access_duration_in_range(cls, access_duration: int) -> int:
+        if access_duration < 900:
+            raise ValueError("The access duration must be at least 900 (seconds).")
+        if access_duration > 43200:
+            raise ValueError(
+                "The access duration for the role session must be smaller than the maximum session "
+                "duration setting for the role, which may at most be 12 hours, "
+                "and thus at most 43200 (seconds)."
+            )
+        return access_duration
 
 
-blob_storage_adapter_config = BlobStorageAdapterConfig()
+environment_file = os.environ.get("HD_RUNTIME_ENVIRONMENT_FILE", None)
+blob_storage_adapter_config = BlobStorageAdapterConfig(_env_file=environment_file)
 
 
 def get_blob_adapter_config() -> BlobStorageAdapterConfig:
