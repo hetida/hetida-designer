f2bb476086093d438e8c71f1af0debc3e5db18d5
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-05-23 14:18:54 +0200

    remove unused classes

--- runtime/hetdesrun/models/__init__.py
+++ runtime/hetdesrun/models/__init__.py
@@ -1,6 +1,5 @@
 """(Pydantic) Models for ingoing / outgoing data"""
 import re
-from typing import Any, Literal, Protocol
 
 from pydantic import ConstrainedStr
 
@@ -10,23 +9,3 @@ RESERVED_FILTER_KEY = ["from", "to", "id"]
 class FilterKey(ConstrainedStr):
     min_length = 1
     regex = re.compile(r"^[a-zA-Z]\w+$", flags=re.ASCII)
-
-
-class FilteredSource(Protocol):
-    """Support for type annotation of custom adapter runtime implementations"""
-
-    ref_id: str | None
-    ref_id_type: Literal["SOURCE", "SINK", "THINGNODE"] | None
-    ref_key: str | None
-    type: str | None  # noqa: A003
-    filters: dict[FilterKey, Any]
-
-
-class FilteredSink(Protocol):
-    """Support for type annotation of custom adapter runtime implementations"""
-
-    ref_id: str | None
-    ref_id_type: Literal["SOURCE", "SINK", "THINGNODE"] | None
-    ref_key: str | None
-    type: str | None  # noqa: A003
-    filters: dict[FilterKey, Any]
