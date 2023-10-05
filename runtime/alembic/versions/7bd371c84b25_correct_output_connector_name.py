eb3fe14e21ae888efd730f1be36370c402886b70
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2023-10-05 14:57:48 +0200

    import instead of redefine

--- runtime/alembic/versions/7bd371c84b25_correct_output_connector_name.py
+++ runtime/alembic/versions/7bd371c84b25_correct_output_connector_name.py
@@ -18,19 +18,13 @@ Workflows that are in the DRAFT state will have their outputs disappear with the
 request and their execution will fail. The affected
 workflows can be automatically repaired using this endpoint instead.
 """
-from enum import StrEnum
-from typing import Any
-from uuid import UUID, uuid4
 
-import sqlalchemy as sa
-from pydantic import BaseModel
-from sqlalchemy import orm
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy_utils import UUIDType
-
-from alembic import op
-
-Base = declarative_base()
+from hetdesrun.persistence.dbservice.revision import (
+    select_multiple_transformation_revisions,
+    update_or_create_single_transformation_revision,
+)
+from hetdesrun.persistence.models.workflow import WorkflowContent
+from hetdesrun.utils import Type
 
 # revision identifiers, used by Alembic.
 revision = "7bd371c84b25"
@@ -39,143 +33,16 @@ branch_labels = None
 depends_on = None
 
 
-class State(StrEnum):
-    DRAFT = "DRAFT"
-    RELEASED = "RELEASED"
-    DISABLED = "DISABLED"
-
-
-class Type(StrEnum):
-    COMPONENT = "COMPONENT"
-    WORKFLOW = "WORKFLOW"
-
-
-class DataType(StrEnum):
-    Integer = "INT"
-    Float = "FLOAT"
-    String = "STRING"
-    DataFrame = "DATAFRAME"
-    Series = "SERIES"
-    MultiTSFrame = "MULTITSFRAME"
-    Boolean = "BOOLEAN"
-    Any = "ANY"
-    PlotlyJson = "PLOTLYJSON"
-
-
-class TransformationRevisionDBModel(Base):
-    __tablename__ = "transformation_revisions"
-
-    id: UUIDType = sa.Column(  # noqa: A003
-        UUIDType(binary=False), primary_key=True, default=uuid4
-    )
-    type = sa.Column(sa.Enum(Type), nullable=False)  # noqa: A003
-    workflow_content = sa.Column(
-        sa.JSON(none_as_null=True), nullable=True, default=lambda: None
-    )
-
-
-class IO(BaseModel):
-    id: UUID  # noqa: A003
-    name: str | None = None
-    data_type: DataType
-
-
-class InputType(StrEnum):
-    REQUIRED = "REQUIRED"
-    OPTIONAL = "OPTIONAL"
-
-
-class InputTypeMixIn(BaseModel):
-    type: InputType = InputType.REQUIRED  # noqa: A003
-    value: Any | None = None
-
-
-class Position(BaseModel):
-    x: int
-    y: int
-
-
-class Connector(IO):
-    position: Position
-
-
-class OperatorInput(InputTypeMixIn, Connector):
-    exposed: bool = False
-
-
-class OperatorOutput(Connector):
-    pass
-
-
-class Operator(BaseModel):
-    id: UUID  # noqa: A003
-    revision_group_id: UUID
-    name: str
-    type: Type  # noqa: A003
-    state: State
-    version_tag: str
-    transformation_id: UUID
-    inputs: list[OperatorInput]
-    outputs: list[OperatorOutput]
-    position: Position
-
-
-class Vertex(BaseModel):
-    """Represents start or end point of a link."""
-
-    operator: UUID | None
-    connector: Connector
-
-
-class Link(BaseModel):
-    id: UUID  # noqa: A003
-    start: Vertex
-    end: Vertex
-    path: list[Position] = []
-
-
-class WorkflowContentIO(Connector):
-    operator_id: UUID
-    connector_id: UUID
-    operator_name: str
-    connector_name: str
-    position: Position = Position(x=0, y=0)
-
-
-class WorkflowContentOutput(WorkflowContentIO):
-    pass
-
-
-class WorkflowContentDynamicInput(InputTypeMixIn, WorkflowContentIO):
-    pass
-
-
-class WorkflowContentConstantInput(WorkflowContentIO):
-    value: str
-
-
-class WorkflowContent(BaseModel):
-    operators: list[Operator] = []
-    links: list[Link] = []
-    constants: list[WorkflowContentConstantInput] = []
-    inputs: list[WorkflowContentDynamicInput] = []
-    outputs: list[WorkflowContentOutput] = []
-
-
 def upgrade() -> None:
-    bind = op.get_bind()
-    session = orm.Session(bind=bind)
+    tr_list = select_multiple_transformation_revisions(type=Type.WORKFLOW)
 
-    for workflow_tr in session.query(TransformationRevisionDBModel).where(
-        TransformationRevisionDBModel.type == Type.WORKFLOW
-    ):
-        wf_content = WorkflowContent(**workflow_tr.workflow_content)
+    for tr in tr_list:
         applicable = False
-        if len(wf_content.outputs) != 0:
-            for output in wf_content.outputs:
+        if isinstance(tr.content, WorkflowContent) and len(tr.content.outputs) != 0:
+            for output in tr.content.outputs:
                 if output.connector_name == "connector_name":
                     applicable = True
-                    for operator in wf_content.operators:
+                    for operator in tr.content.operators:
                         if operator.id == output.operator_id:
                             for output_connector in operator.outputs:
                                 if output_connector.id == output.connector_id:
@@ -184,7 +51,7 @@ def upgrade() -> None:
                                     )
                                     output.connector_name = output_connector.name
         if applicable:
-            update_or_create_transformation_revision(tr, directly_in_db=directly_in_db)
+            update_or_create_single_transformation_revision(tr)
 
 
 def downgrade() -> None:
