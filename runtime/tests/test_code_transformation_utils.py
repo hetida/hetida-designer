8df71e4cb94031c2166b9e9c26b782406b4759ba
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2024-01-05 12:19:43 +0100

    add unit tests for update module level variable

--- runtime/tests/test_code_transformation_utils.py
+++ runtime/tests/test_code_transformation_utils.py
@@ -1,13 +1,18 @@
 import pytest
 
-from hetdesrun.component.code_utils import CodeParsingException, format_code_with_black
+from hetdesrun.component.code_utils import (
+    CodeParsingException,
+    format_code_with_black,
+    update_module_level_variable,
+)
 from hetdesrun.trafoutils.io.load import load_python_file
 
 
 @pytest.fixture()
 def example_component_code() -> str:
-    component_code_path = "tests/data/components/reduced_code.py"
+    component_code_path = "tests/data/components/expanded_code.py"
     return load_python_file(component_code_path)
+
 
 def test_format_code_with_black(example_component_code: str):
     unformatted_component_code = example_component_code.replace(" = ", "=")
@@ -24,3 +29,34 @@ def test_format_code_with_syntax_error_with_black(example_component_code: str):
 
     with pytest.raises(CodeParsingException):
         format_code_with_black(component_code_with_syntax_error)
+
+
+def test_add_module_level_variable(example_component_code: str):
+    updated_code = update_module_level_variable(
+        code=example_component_code, variable_name="TEST", value=None
+    )
+
+    assert "TEST = None" in updated_code
+
+
+def test_update_module_level_variable(example_component_code: str):
+    updated_code = update_module_level_variable(
+        code=example_component_code,
+        variable_name="TEST_WIRING_FROM_PY_FILE_IMPORT",
+        value={"input_wirings": []},
+    )
+
+    assert '"adapter_id": "direct_provisioning"' not in updated_code
+    assert 'TEST_WIRING_FROM_PY_FILE_IMPORT = {"input_wirings": []}\n' in updated_code
+
+
+def test_update_module_level_variable_in_code_with_syntax_error(example_component_code: str):
+    component_code_with_syntax_error = example_component_code.replace("):", ")")
+    assert component_code_with_syntax_error != example_component_code
+
+    with pytest.raises():
+        update_module_level_variable(
+            code=component_code_with_syntax_error,
+            variable_name="TEST_WIRING_FROM_PY_FILE_IMPORT",
+            value={"input_wirings": []},
+        )
