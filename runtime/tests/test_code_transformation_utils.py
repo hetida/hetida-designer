fdf12736a88f88f3ae3ad932d9865946d1de6746
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2024-01-05 12:04:14 +0100

    use a fixture to get the example_code

--- runtime/tests/test_code_transformation_utils.py
+++ runtime/tests/test_code_transformation_utils.py
@@ -4,22 +4,23 @@ from hetdesrun.component.code_utils import CodeParsingException, format_code_wit
 from hetdesrun.trafoutils.io.load import load_python_file
 
 
-def test_format_code_with_black():
+@pytest.fixture()
+def example_component_code() -> str:
     component_code_path = "tests/data/components/reduced_code.py"
-    component_code = load_python_file(component_code_path)
-    unformatted_component_code = component_code.replace(" = ", "=")
-    assert unformatted_component_code != component_code
+    return load_python_file(component_code_path)
+
+def test_format_code_with_black(example_component_code: str):
+    unformatted_component_code = example_component_code.replace(" = ", "=")
+    assert unformatted_component_code != example_component_code
 
     formatted_code = format_code_with_black(unformatted_component_code)
     assert formatted_code != unformatted_component_code
-    assert formatted_code == component_code
+    assert formatted_code == example_component_code
 
 
-def test_format_code_with_syntax_error_with_black():
-    component_code_path = "tests/data/components/reduced_code.py"
-    component_code = load_python_file(component_code_path)
-    component_code_with_syntax_error = component_code.replace("):", ")")
-    assert component_code_with_syntax_error != component_code
+def test_format_code_with_syntax_error_with_black(example_component_code: str):
+    component_code_with_syntax_error = example_component_code.replace("):", ")")
+    assert component_code_with_syntax_error != example_component_code
 
     with pytest.raises(CodeParsingException):
         format_code_with_black(component_code_with_syntax_error)
