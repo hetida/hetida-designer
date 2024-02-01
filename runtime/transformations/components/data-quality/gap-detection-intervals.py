b7f69c7116d83361288f0e049433555b7956b2ae
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2024-02-01 19:01:54 +0100

    re-order inputs, some fixes
    - correct doctring
    - fix validators
    - fix type annotations

--- runtime/transformations/components/data-quality/gap-detection-intervals.py
+++ runtime/transformations/components/data-quality/gap-detection-intervals.py
@@ -22,42 +22,47 @@ information about the gaps.
   the processing range ends with the last data point in `timeseries`. The start date must not be
   later than the end date.
 
-- **auto_stepsize** (Boolean):
-  If True, the function will automatically determine the step unit based on the timeseries data. If
-  False, a `step_size_str` must be set.
+- **auto_stepsize** (Boolean, default value: true):
+  If True, the function will automatically determine the step size based on the `timeseries` index
+  and no `step_size_str` may be set.
+  If False, a `step_size_str` must be set.
 
 - **history_end_date_str** (String, default value: null):
-  Expects a date between start_date and end_date in iso format.
+  Only relevant when `auto_stepsize` is True.
+  Expects a date between start_date and end_date in ISO 8601 format.
   The desired end date for the training data used to determine the step unit.
-  This is only relevant when auto_stepsize is True. If not specified, the entire
+  If not specified, the entire
   processing range is used as training data.
 
-- **percentil** (Float, default value: 0.5):
+- **percentile** (Float, default value: 0.5):
+  Only relevant when `auto_stepsize` is True.
   Expects a positive value smaller than or equal to 1. The percentile value to use for automatic
   determination of the expected gapsize between two consecutive data points. The value should be
   selected according to the expected ratio of gaps to regular steps. I.e. if it is to be expected
   that more than 50% of the time steps are gaps, the value should be reduced; if fewer gaps are to
-  be expected, the value can be adjusted upwards accordingly. This is only relevant when
-  auto_stepsize is True.
+  be expected, the value can be adjusted upwards accordingly.
 
-- **min_amount_datapoints** (Int, default value: 21):
-  Expects a positive value. Minimum amount of datapoints required for a feasible result. Depends on
-  the `percentil` value. This is only relevant when auto_stepsize is True.
-  Some examples for the value pairs are:
-  - `percentil` = 0.5, `min_amount_datapoints` = 3
-  - `percentil` = 0.1, `min_amount_datapoints` = 11
-  - `percentil` = 0.55, `min_amount_datapoints` = 21
-  - `percentil` = 0.51, `min_amount_datapoints` = 101
+- **interpolation_method** (String, default value: nearest):
+  Only relevant when `auto_stepsize` is True.
+  Is used to determine the percentile value via the Pandas `quantile` method.
+  Thus, must be one of `linear`, `lower`, `higher`, `midpoint`, `nearest`.
+
+- **min_amount_datapoints** (Int, default value: 11):
+  Only relevant when `auto_stepsize` is True.
+  Expects a positive value. Minimum amount of datapoints required. Effects the expectable precision
+  of the `percentile` value.
 
 - **step_size_str** (String, default value: null):
+  Must not be set if `auto_stepsize` is True, but must be set if `auto_stepsize` is False.
   The expected time step between consecutive timestamps in the time series. Must be a frequency
   string, e.g. "D" or "60s".
 
 - **step_size_factor** (Float, default value: 1.0):
   Expects a positive value. The value is used to define when a step between two consecutive data
   points is recognized as a gap. I.e. a value of 1.0 means that all steps larger than the
-  specified `step_size` are recognized as gaps, while a value of 2.0 means that steps are only
-  recognized as gaps if they are more than twice as large as the specified `step_size`.
+  specified or determined step size are recognized as gaps, while a value of 2.0 means that steps
+  are only recognized as gaps if they are more than twice as large as the specified or determined
+  step size.
 
 
 ## Outputs
@@ -80,7 +85,7 @@ information about the gaps.
 If the `start_date_str` (`end_date_str`) is defined, the component checks that it is present in the
 `series` and removes any data points with an earlier (later) index from the processing range.
 To detect gaps the time steps between consecutive data points are calculated. If `auto_stepsize` is
-set to `True` the component determines the `step_size` using the `percentil`th quantile of time
+set to `True` the component determines the `step_size` using the `percentile`th quantile of time
 steps in the training range. The training range is determined by the `start_date_str` and the
 `history_end_date_str`. If latter is not defined, the processing range is used as training range. If
 `auto_step_size` is set to `False` the `step_size` is defined by the `step_size_str`. When a time
@@ -91,8 +96,6 @@ corresponding values. If both the value to the left and right of the gap are def
 type float or int, the arithmetic mean of the two values is also output.
 """
 
-
-from datetime import datetime, timedelta
 from typing import Any
 
 import numpy as np
@@ -102,20 +105,42 @@ from pydantic import BaseModel, validator
 from hetdesrun.runtime.exceptions import ComponentInputValidationException
 
 
+def freqstr2dateoffset(freqstr: str) -> pd.DateOffset:
+    """Transform frequency string to Pandas DateOffset."""
+    return pd.tseries.frequencies.to_offset(freqstr)
+
+
+def freqstr2timedelta(freqstr: str, input_name: str) -> pd.Timedelta:
+    """Transform frequency string to Pandas Timedelta."""
+    try:
+        return pd.to_timedelta(freqstr)
+    except ValueError:
+        try:
+            return pd.to_timedelta(freqstr2dateoffset(freqstr))
+        except TypeError as err:
+            raise ComponentInputValidationException(
+                f"The step_size_str cannot be parsed as Pandas Timedelta: {str(err)}",
+                error_code=422,
+                invalid_component_inputs=[input_name],
+            ) from err
+
+
 class GapDetectionParameters(BaseModel):
-    start_date: pd.Timestamp | None  # pydantic kann auch direkt datetime, muss aber getestet werden
+    start_date: pd.Timestamp | None
     end_date: pd.Timestamp | None
     auto_stepsize: bool
     history_end_date: pd.Timestamp | None
-    step_size_str: str
-    percentil: float
-    step_size_factor: float
-    min_amount_datapoints: int
+    percentile: float
     interpolation_method: str
+    min_amount_datapoints: int
+    step_size_str: str | None
+    step_size_factor: float
     fill_value: Any
 
     @validator("end_date")
-    def verify_dates(cls, end_date, values: dict):
+    def verify_dates(
+        cls, end_date: pd.Timestamp | None, values: dict
+    ) -> pd.Timestamp | None:
         start_date = values["start_date"]
         if None not in (start_date, end_date) and start_date > end_date:
             raise ComponentInputValidationException(
@@ -127,7 +152,9 @@ class GapDetectionParameters(BaseModel):
         return end_date
 
     @validator("history_end_date")
-    def verify_history_end_date(cls, history_end_date, values: dict) -> datetime | None:
+    def verify_history_end_date(
+        cls, history_end_date: pd.Timestamp | None, values: dict
+    ) -> pd.Timestamp | None:
         start_date = values["start_date"]
         end_date = values["end_date"]
         if history_end_date is not None:
@@ -149,43 +176,66 @@ class GapDetectionParameters(BaseModel):
             history_end_date = None
         return history_end_date
 
+    @validator("percentile")
+    def verify_percentile(cls, percentile: float) -> float:
+        if (percentile < 0) or (percentile > 1):
+            raise ComponentInputValidationException(
+                "The percentile value has to be a non-negative float less or equal to 1.",
+                error_code=422,
+                invalid_component_inputs=["percentile"],
+            )
+        return percentile
+
+    @validator("interpolation_method")
+    def verify_interpolation_method(cls, interpolation_method: str) -> str:
+        if interpolation_method not in (
+            "linear",
+            "lower",
+            "higher",
+            "midpoint",
+            "nearest",
+        ):
+            raise ComponentInputValidationException(
+                "The interpolation method must be one of "
+                "`linear`, `lower`, `higher`, `midpoint`, `nearest`",
+                error_code=422,
+                invalid_component_inputs=["interpolation_method"],
+            )
+        return interpolation_method
+
+    @validator("min_amount_datapoints")
+    def verify_min_amount_datapoints(cls, min_amount_datapoints: int) -> int:
+        if min_amount_datapoints < 0:
+            raise ComponentInputValidationException(
+                "The minimum amount of datapoints has to be a non-negative integer.",
+                error_code=422,
+                invalid_component_inputs=["min_amount_datapoints"],
+            )
+        return min_amount_datapoints
+
     @validator("step_size_str")
-    def verify_step_size(cls, step_size, values: dict) -> str:
+    def verify_step_size(cls, step_size_str: str, values: dict) -> str:
         auto_stepsize = values["auto_stepsize"]
         if auto_stepsize is False:
-            if step_size is None:
+            if step_size_str is None:
                 raise ComponentInputValidationException(
                     "A step_size is required for gap detection, "
                     "if it is not automatically determined.",
                     error_code=422,
                     invalid_component_inputs=["step_size_str"],
                 )
-            try:
-                try:
-                    _ = pd.to_timedelta(step_size)
-                except ValueError:
-                    _ = pd.to_timedelta(pd.tseries.frequencies.to_offset(step_size))
-            except ValueError as err:
-                raise ComponentInputValidationException(
-                    "A step_size is required for gap detection, "
-                    "if it is not automatically determined.",
-                    error_code=422,
-                    invalid_component_inputs=["step_size_str"],
-                ) from err
-        return step_size
-
-    @validator("percentil")
-    def verify_percentile(cls, percentil) -> float:
-        if (percentil < 0) or (percentil > 1):
+            freqstr2timedelta(step_size_str, "step_size_str")
+        elif step_size_str is not None:
             raise ComponentInputValidationException(
-                "The percentil value has to be a non-negative float less or equal to 1.",
+                "The step_size must not be set, "
+                "if automatical step size determination is enabled.",
                 error_code=422,
-                invalid_component_inputs=["percentil"],
+                invalid_component_inputs=["step_size_str"],
             )
-        return percentil
+        return step_size_str
 
     @validator("step_size_factor")
-    def verify_step_size_factor(cls, factor) -> float:
+    def verify_step_size_factor(cls, factor: float) -> float:
         if factor < 0:
             raise ComponentInputValidationException(
                 "The gap size factor has to be a non-negative float.",
@@ -194,42 +244,22 @@ class GapDetectionParameters(BaseModel):
             )
         return factor
 
-    @validator("min_amount_datapoints")
-    def verify_min_amount_datapoints(cls, min_amount) -> int:
-        if min_amount < 0:
-            raise ComponentInputValidationException(
-                "The minimum amount of datapoints has to be a non-negative integer.",
-                error_code=422,
-                invalid_component_inputs=["min_amount_datapoints"],
-            )
-        return min_amount
 
-
-def timestamp_str_to_pd_timestamp(timestamp: str) -> datetime:
+def timestamp_str_to_pd_timestamp(timestamp: str, input_name: str) -> pd.Timestamp:
     try:
         date = pd.to_datetime(timestamp, utc=True)
     except ValueError as error:
         raise ComponentInputValidationException(
-            str(error), error_code=422, invalid_component_inputs=["..."]
+            str(error), error_code=422, invalid_component_inputs=[input_name]
         ) from error
     return date
 
 
-def freqstr2dateoffset(freqstr: str) -> pd.DateOffset:
-    """Transform frequency string to Pandas DateOffset."""
-    return pd.tseries.frequencies.to_offset(freqstr)
-
-
-def freqstr2timedelta(freqstr: str) -> pd.Timedelta:
-    """Transform frequency string to Pandas Timedelta."""
-    try:
-        return pd.to_timedelta(freqstr)
-    except ValueError:
-        return pd.to_timedelta(freqstr2dateoffset(freqstr))
-
-
 def check_add_boundary_dates(
-    timeseries: pd.Series, start_date: datetime, end_date: datetime, fill_value=None
+    timeseries: pd.Series,
+    start_date: pd.Timestamp,
+    end_date: pd.Timestamp,
+    fill_value: Any | None = None,
 ) -> pd.Series:
     if start_date not in timeseries.index:
         timeseries[start_date] = fill_value
@@ -257,7 +287,7 @@ def constrict_series_to_dates(
     return timeseries_data[series_after_start & series_before_end]
 
 
-def check_amount_datapoints(series: pd.Series, min_amount_datapoints: int):
+def check_amount_datapoints(series: pd.Series, min_amount_datapoints: int) -> None:
     if len(series) < min_amount_datapoints:
         raise ComponentInputValidationException(
             f"The timeseries must contain at least {min_amount_datapoints} datapoints.",
@@ -268,22 +298,22 @@ def check_amount_datapoints(series: pd.Series, min_amount_datapoints: int):
 
 def determine_timestep_gapsize_percentile(
     timeseries_data: pd.Series | pd.DataFrame,
-    percentil: float,
+    percentile: float,
     interpolation_method: str,
 ) -> pd.Timedelta:
     gaps = timeseries_data.index.to_series().diff().dropna()
 
-    percentile_gapsize = gaps.quantile(percentil, interpolation=interpolation_method)
+    percentile_gapsize = gaps.quantile(percentile, interpolation=interpolation_method)
 
     return percentile_gapsize
 
 
 def determine_gap_length(
-    timeseries: pd.Series, stepsize=timedelta(minutes=1)
+    timeseries: pd.Series, step_size: pd.Timedelta
 ) -> pd.DataFrame:
     gaps = timeseries.index.to_series().diff().to_numpy()
 
-    stepsize_seconds = stepsize.total_seconds()
+    stepsize_seconds = step_size.total_seconds()
 
     normalized_gaps = [
         pd.Timedelta(gap).total_seconds() / stepsize_seconds if pd.notna(gap) else None
@@ -298,7 +328,7 @@ def determine_gap_length(
 
 
 def return_gap_boundary_timestamps(
-    frame_with_gapsizes: pd.DataFrame, series: pd.Series, step_size_factor=1.0
+    frame_with_gapsizes: pd.DataFrame, series: pd.Series, step_size_factor: float = 1.0
 ) -> pd.DataFrame:
     # Identify rows where gap is greater than 1
     large_gap_indices = frame_with_gapsizes[
@@ -340,14 +370,14 @@ COMPONENT_INFO = {
         "timeseries": {"data_type": "SERIES"},
         "start_date_str": {"data_type": "STRING", "default_value": None},
         "end_date_str": {"data_type": "STRING", "default_value": None},
-        "auto_stepsize": {"data_type": "BOOLEAN"},
+        "auto_stepsize": {"data_type": "BOOLEAN", "default_value": True},
         "history_end_date_str": {"data_type": "STRING", "default_value": None},
-        "percentil": {"data_type": "FLOAT", "default_value": 0.5},
+        "percentile": {"data_type": "FLOAT", "default_value": 0.5},
         "interpolation_method": {"data_type": "STRING", "default_value": "nearest"},
+        "min_amount_datapoints": {"data_type": "INT", "default_value": 11},
         "step_size_str": {"data_type": "STRING", "default_value": None},
         "step_size_factor": {"data_type": "FLOAT", "default_value": 1.0},
         "fill_value": {"data_type": "ANY", "default_value": None},
-        "min_amount_datapoints": {"data_type": "INT", "default_value": 21},
     },
     "outputs": {
         "gap_info": {"data_type": "DATAFRAME"},
@@ -365,16 +395,16 @@ COMPONENT_INFO = {
 def main(
     *,
     timeseries,
-    auto_stepsize,
     start_date_str=None,
     end_date_str=None,
+    auto_stepsize=True,
     history_end_date_str=None,
-    percentil=0.5,
+    percentile=0.5,
     interpolation_method="nearest",
+    min_amount_datapoints=21,
     step_size_str=None,
     step_size_factor=1.0,
     fill_value=None,
-    min_amount_datapoints=21,
 ):
     # entrypoint function for this component
     # ***** DO NOT EDIT LINES ABOVE *****
@@ -392,12 +422,15 @@ def main(
         elif "to" in timeseries.attrs:
             end_date_str = timeseries.attrs["to"]
 
-    if step_size_str is None and "ref_frequency" in timeseries.attrs:
+    if step_size_str is None and not auto_stepsize and "ref_frequency" in timeseries.attrs:
         step_size_str = timeseries.attrs["ref_frequency"]
+        auto_stepsize = False
 
-    start_date = timestamp_str_to_pd_timestamp(start_date_str)
-    end_date = timestamp_str_to_pd_timestamp(end_date_str)
-    history_end_date = timestamp_str_to_pd_timestamp(history_end_date_str)
+    start_date = timestamp_str_to_pd_timestamp(start_date_str, "start_date_str")
+    end_date = timestamp_str_to_pd_timestamp(end_date_str, "end_date_str")
+    history_end_date = timestamp_str_to_pd_timestamp(
+        history_end_date_str, "history_end_date_str"
+    )
 
     input_params = GapDetectionParameters(
         start_date=start_date,
@@ -405,7 +438,7 @@ def main(
         auto_stepsize=auto_stepsize,
         history_end_date_str=history_end_date,
         step_size_str=step_size_str,
-        percentil=percentil,
+        percentile=percentile,
         min_amount_datapoints=min_amount_datapoints,
         interpolation_method=interpolation_method,
         fill_value=fill_value,
@@ -430,7 +463,7 @@ def main(
         else:
             training_series = constricted_series
         step_size = determine_timestep_gapsize_percentile(
-            training_series, percentil, interpolation_method
+            training_series, percentile, interpolation_method
         )
     else:
         step_size = freqstr2timedelta(step_size_str)
