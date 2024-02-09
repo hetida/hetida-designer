526337983e4b875ace468db965683aafb99b4ca5
Miriam Kuemmel <mkuemmel@neusta-sd-west.de>
2024-02-09 10:06:37 +0100

    Revert "fix type hints"
    This reverts commit c61a9eb008922dcded7b212490019e9cba945740.

--- runtime/hetdesrun/wiring/__init__.py
+++ runtime/hetdesrun/wiring/__init__.py
@@ -1,8 +1,9 @@
+from collections import defaultdict
 from typing import Any
 
 from hetdesrun.adapters import load_data_from_adapter, send_data_with_adapter
 from hetdesrun.models.data_selection import FilteredSink, FilteredSource
-from hetdesrun.models.wiring import InputWiring, OutputWiring, WorkflowWiring
+from hetdesrun.models.wiring import WorkflowWiring
 
 
 async def resolve_and_load_data_from_wiring(
@@ -13,7 +14,7 @@ async def resolve_and_load_data_from_wiring(
     Data is loaded in batches per adapter.
     """
 
-    wirings_by_adapter: dict[Any, list[InputWiring]] = {}
+    wirings_by_adapter = defaultdict(list)
 
     for input_wiring in workflow_wiring.input_wirings:
         if input_wiring.use_default_value is False:
@@ -50,7 +51,7 @@ async def resolve_and_send_data_from_wiring(
     Data that is not send to a sink by the workflow wiring is returned.
     """
 
-    wirings_by_adapter: dict[Any, list[OutputWiring]] = {}
+    wirings_by_adapter = defaultdict(list)
 
     for output_wiring in workflow_wiring.output_wirings:
         wirings_by_adapter[output_wiring.adapter_id].append(output_wiring)
