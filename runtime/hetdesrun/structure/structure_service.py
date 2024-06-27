a98de0fd49f20c7c69a45d1458e6430667f79b30
j0sek1 <shindel@fuseki.com>
2024-06-27 08:30:07 +0200

    add get_item function

--- runtime/hetdesrun/structure/structure_service.py
+++ runtime/hetdesrun/structure/structure_service.py
@@ -1,12 +1,14 @@
 from uuid import UUID
 
-from hetdesrun.structure.db import get_session
 from hetdesrun.persistence.dbmodels import SinkOrm, SourceOrm, ThingNodeOrm
-from hetdesrun.structure.models import Sink, Source, ThingNode
+from hetdesrun.structure.db import get_session
 from hetdesrun.structure.db.exceptions import DBNotFoundError
+from hetdesrun.structure.models import Sink, Source, ThingNode
 
 
-def get_children(parent_id: UUID | None) -> tuple[list[ThingNode], list[Source], list[Sink]]:
+def get_children(
+    parent_id: UUID | None,
+) -> tuple[list[ThingNode], list[Source], list[Sink]]:
     with get_session()() as session:
         if parent_id is None:
             root_nodes = (
@@ -25,15 +27,36 @@ def get_children(parent_id: UUID | None) -> tuple[list[ThingNode], list[Source],
         sources = (
             session.query(SourceOrm).filter(SourceOrm.thing_node_id == parent_id).all()
         )
-        sinks = (
-            session.query(SinkOrm).filter(SinkOrm.thing_node_id == parent_id).all()
-        )
+        sinks = session.query(SinkOrm).filter(SinkOrm.thing_node_id == parent_id).all()
 
         if not child_nodes and not sources and not sinks:
-            raise DBNotFoundError(f"No children, sources, or sinks found for parent_id {parent_id}")
+            raise DBNotFoundError(
+                f"No children, sources, or sinks found for parent_id {parent_id}"
+            )
 
         return (
             [ThingNode.from_orm_model(node) for node in child_nodes],
             [Source.from_orm_model(source) for source in sources],
-            [Sink.from_orm_model(sink) for sink in sinks]
+            [Sink.from_orm_model(sink) for sink in sinks],
+        )
+
+
+def get_item(item_id: UUID) -> ThingNode | Source | Sink:
+    with get_session()() as session:
+        thing_node = (
+            session.query(ThingNodeOrm).filter(ThingNodeOrm.id == item_id).one_or_none()
+        )
+        if thing_node:
+            return ThingNode.from_orm_model(thing_node)
+
+        source = session.query(SourceOrm).filter(SourceOrm.id == item_id).one_or_none()
+        if source:
+            return Source.from_orm_model(source)
+
+        sink = session.query(SinkOrm).filter(SinkOrm.id == item_id).one_or_none()
+        if sink:
+            return Sink.from_orm_model(sink)
+
+        raise DBNotFoundError(
+            f"No ThingNode, Source, or Sink found for item_id {item_id}"
         )
