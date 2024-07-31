3a96ab97ea8ea694b4969ab528b4cfe49e229622
Fabian Wetjen <fwetjen@neusta-sd-west.de>
2024-07-31 12:06:48 +0200

    Provide more detailed description for structure_prepopulation_var and fix formatof caching_var description

--- runtime/hetdesrun/webservice/config.py
+++ runtime/hetdesrun/webservice/config.py
@@ -118,8 +118,8 @@ class RuntimeConfig(BaseSettings):
         False,
         env="HD_ENABLE_CACHING_FOR_NON_DRAFT_TRAFOS_FOR_EXEC",
         description=(
-            "Cache transformation revisions for execution if their state is not DRAFT."
-            "Instead of always loading them from the database."
+            "Cache transformation revisions for execution if their state is not DRAFT. "
+            "Instead of always loading them from the database. "
             "The caching mechanism is NOT thread-safe."
         ),
     )
@@ -323,11 +323,16 @@ class RuntimeConfig(BaseSettings):
         env="HD_BACKEND_AUTOIMPORT_DIRECTORY",
     )
 
-    # TODO More detailed description is probably necessary
     structure_to_prepopulate_virtual_structure_adapter: CompleteStructure | None = Field(
         None,
-        description="A structure in JSON-format."
-        "Containing all thingnodes, sources, sinks and element types of the users data",
+        description="A JSON, used to provide a structure for the virtual structure adapter "
+        "at hetida designer startup. "
+        "This built-in adapter enables the user to create "
+        "a flexible, abstract hierarchical structure for their data. "
+        "In this JSON the user can provide names, descriptions and metadata "
+        "for each element of the hierarchy. "
+        "The JSON should contain definitions for all thingnodes, sources, sinks and element types "
+        "representing the users data.",
         env="STRUCTURE_TO_PREPOPULATE_VST_ADAPTER",
     )
 
