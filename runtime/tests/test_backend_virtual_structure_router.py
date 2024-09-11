4cdb40c51a7a399252f93cfe2d5c0abfad211af9
Fabian Wetjen <fwetjen@neusta-sd-west.de>
2024-09-11 10:28:46 +0200

    Implement basic structure router functionality

---
+++ runtime/tests/test_backend_virtual_structure_router.py
@@ -0,0 +1,14 @@
+import json
+
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_update_structure(async_test_client, mocked_clean_test_db_session):
+    file_path = "tests/virtual_structure_adapter/data/simple_end_to_end_test.json"
+    with open(file_path) as file:
+        structure_json = json.load(file)
+
+    async with async_test_client as ac:
+        response = await ac.put("/api/structure/update/", json=structure_json)
+    assert response.status_code == 204
