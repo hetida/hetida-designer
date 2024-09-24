d32bd3fb287ea4e38073cd7d9aa289cf5fbefd0b
stnhdl <shindel@fuseki.com>
2024-09-24 08:02:03 +0200

    Revert function `test_update_structure_with_invalid_structure` to its original state.

--- runtime/tests/test_backend_virtual_structure_router.py
+++ runtime/tests/test_backend_virtual_structure_router.py
@@ -24,9 +24,5 @@ async def test_update_structure_with_invalid_structure(
 
     async with async_test_client as ac:
         response = await ac.put("/api/structure/update/", json=structure_json)
-
-    assert response.status_code == 422, f"Unexpected status code: {response.status_code}"
-    assert (
-        "Duplicate external_id 'Waterworks1' found in thing_nodes."
-        in response.json()["detail"][0]["msg"]
-    ), "Expected validation error message was not found in the response."
+    assert response.status_code == 500, f"Unexpected status code: {response.status_code}"
+    assert "Integrity Error while updating or inserting the structure" in response.json()["detail"]
