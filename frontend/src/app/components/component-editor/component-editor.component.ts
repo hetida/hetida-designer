f7b6c02f41ba0a08da5ade3d2b3e4624890698d3
pka1024 <pkasek@fuseki.com>
2025-02-07 14:31:21 +0100

    angular 18 update
    feat(angular 18 update):
    - nodejs version changed to the latest LTS v22.13.0.
    - updated hetida-flowchart packages.
    - fixed a subscription bug in the toolbar.
    - fixed two timers to unsubscribe on closed components and not run further in the background.
    - removed "compile-libs": "ngcc" from package.json, it's no longer supported since Angular 16.

--- frontend/src/app/components/component-editor/component-editor.component.ts
+++ frontend/src/app/components/component-editor/component-editor.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { of, Subject } from 'rxjs';
-import { debounceTime, switchMap, takeUntil } from 'rxjs/operators';
+import { debounceTime, switchMap } from 'rxjs/operators';
 import { RevisionState } from 'src/app/enums/revision-state';
 import { ThemeService } from 'src/app/service/theme/theme.service';
 import { environment } from '../../../environments/environment';
@@ -12,7 +13,7 @@ import { TransformationService } from '../../service/transformation/transformati
   templateUrl: './component-editor.component.html',
   styleUrls: ['./component-editor.component.scss']
 })
-export class ComponentEditorComponent implements OnInit, OnDestroy {
+export class ComponentEditorComponent implements OnInit {
   public editorOptions = {
     theme: 'vs-dark',
     language: 'python',
@@ -24,7 +25,6 @@ export class ComponentEditorComponent implements OnInit, OnDestroy {
   public codeCopy: string;
   public lastSavedCode: string;
 
-  private readonly _ngDestroyNotifier = new Subject<void>();
   private readonly _autoSave$ = new Subject<void>();
   private readonly _autoSaveTimer$ = this._autoSave$.pipe(
     debounceTime(environment.autosaveTimer)
@@ -36,6 +36,7 @@ export class ComponentEditorComponent implements OnInit, OnDestroy {
   ]);
 
   private _componentTransformation: ComponentTransformation;
+  private readonly _destroyRef = inject(DestroyRef);
 
   @Input()
   set componentTransformation(
@@ -62,14 +63,9 @@ export class ComponentEditorComponent implements OnInit, OnDestroy {
     private readonly themeService: ThemeService
   ) {}
 
-  ngOnDestroy(): void {
-    this._ngDestroyNotifier.next();
-    this._ngDestroyNotifier.complete();
-  }
-
   ngOnInit() {
     this.themeService.currentTheme
-      .pipe(takeUntil(this._ngDestroyNotifier))
+      .pipe(takeUntilDestroyed(this._destroyRef))
       .subscribe(theme => {
         this.editorOptions = {
           ...this.editorOptions,
