05421378723f939723102b4256962a9e1b3bd260
stewit <you@example.com>
2025-06-24 15:10:11 +0200

    fix handling of newlines in STRING outputs or in json rep of ANY outputs

--- frontend/src/app/components/protocol-viewer/protocol-viewer.component.ts
+++ frontend/src/app/components/protocol-viewer/protocol-viewer.component.ts
@@ -49,6 +49,8 @@ export class ProtocolViewerComponent implements AfterViewInit {
   plotlyTemplate: TemplateRef<any>;
   @ViewChild('simpleTemplate', { static: true })
   simpleTemplate: TemplateRef<any>;
+  @ViewChild('stringTemplate', { static: true })
+  stringTemplate: TemplateRef<any>;
 
   @HostBinding('style.height') hostHeight = this.HOST_HEIGHT_SHRINKED;
   @HostBinding('class.visible') get visible() {
@@ -102,10 +104,11 @@ export class ProtocolViewerComponent implements AfterViewInit {
             : (this.visible = true)
         ),
         filter(protocol => protocol !== undefined),
-        map(protocol => protocol.replace(/\\n/gm, '\n')),
+        //map(protocol => protocol.replace(/\\n/gm, '\n')),
         tap(protocol => (this.executionResponseRaw = protocol)),
         map(protocol => JSON.parse(protocol) as ExecutionResponse),
         catchError(() => {
+          console.error('Parsing execution response error.');
           return of(this.executionResponseRaw);
         }),
         onErrorResumeNext()
@@ -117,6 +120,9 @@ export class ProtocolViewerComponent implements AfterViewInit {
     this.lastProtocol$.subscribe(executionResponse => {
       if (typeof executionResponse === 'string') {
         this.displayRawValue = true;
+        console.error(
+          'executionResponse is type string, could not parse as json'
+        );
       } else {
         this.displayRawValue = false;
         this.executionResponse = executionResponse;
@@ -131,6 +137,8 @@ export class ProtocolViewerComponent implements AfterViewInit {
 
     if (this.outputIsPlotlyJson(outputKey)) {
       template = this.plotlyTemplate;
+    } else if (this.outputIsString(outputKey)) {
+      template = this.stringTemplate;
     } else {
       template = this.simpleTemplate;
     }
@@ -149,6 +157,13 @@ export class ProtocolViewerComponent implements AfterViewInit {
       this.executionResponse.output_types_by_output_name[outputKey];
     // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
     return resultType === IOType.PLOTLYJSON;
+  }
+
+  outputIsString(outputKey: string): boolean {
+    const resultType =
+      this.executionResponse.output_types_by_output_name[outputKey];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+    return resultType === IOType.STRING;
   }
 
   stringifyJson(value: any) {
