69596a65362d8949820905dfe31f779e4e8db76e
stewit <you@example.com>
2025-08-06 16:18:46 +0200

    frontend first changes showing deprecated revisions

--- frontend/src/app/components/navigation/navigation-container/navigation-container.component.ts
+++ frontend/src/app/components/navigation/navigation-container/navigation-container.component.ts
@@ -15,89 +15,93 @@ import { KeyValue } from '@angular/common';
 import { Utils } from '../../../utils/utils';
 
 @Component({
-    selector: 'hd-navigation-container',
-    templateUrl: './navigation-container.component.html',
-    styleUrls: ['./navigation-container.component.scss']
+  selector: 'hd-navigation-container',
+  templateUrl: './navigation-container.component.html',
+  styleUrls: ['./navigation-container.component.scss']
 })
 export class NavigationContainerComponent implements OnInit {
-    constructor(
-        private readonly transformationStore: Store<TransformationState>,
-        private readonly transformationService: TransformationService,
-        private readonly popoverService: PopoverService,
-        private readonly transformationActionService: TransformationActionService,
-        private readonly authService: AuthService
-    ) { }
+  constructor(
+    private readonly transformationStore: Store<TransformationState>,
+    private readonly transformationService: TransformationService,
+    private readonly popoverService: PopoverService,
+    private readonly transformationActionService: TransformationActionService,
+    private readonly authService: AuthService
+  ) {}
 
-    readonly searchFilter = new FormControl('');
-    readonly typeFilter = new FormControl(TransformationType.WORKFLOW);
+  readonly searchFilter = new FormControl('');
+  readonly typeFilter = new FormControl(TransformationType.WORKFLOW);
 
-    readonly showDeprecatedFilter = new FormControl(false);
+  readonly showDeprecatedFilter = new FormControl(false);
 
-    transformationsByCategory: { [category: string]: Transformation[] };
+  transformationsByCategory: { [category: string]: Transformation[] };
 
-    getFilterState(type: string): boolean {
-        return (this.typeFilter.value as string) === type;
-    }
+  getFilterState(type: string): boolean {
+    return (this.typeFilter.value as string) === type;
+  }
 
-    get filterChanges(): Observable<TransformationType> {
-        return this.typeFilter.valueChanges;
-    }
+  get filterChanges(): Observable<TransformationType> {
+    return this.typeFilter.valueChanges;
+  }
 
-    get searchFilterChanges(): Observable<string> {
-        return this.searchFilter.valueChanges;
-    }
+  get searchFilterChanges(): Observable<string> {
+    return this.searchFilter.valueChanges;
+  }
 
-    get deprecatedFilterChanges(): Observable<boolean> {
-        return this.showDeprecatedFilter.valueChanges;
-    }
+  get deprecatedFilterChanges(): Observable<boolean> {
+    return this.showDeprecatedFilter.valueChanges;
+  }
 
-    trackCategory(_index: number, category: any) {
-        return category.key;
-    }
+  trackCategory(_index: number, category: any) {
+    return category.key;
+  }
 
-    ngOnInit(): void {
-        this.authService.isAuthenticated$().subscribe(() => {
-            this.transformationService.fetchAllTransformations();
-        });
+  ngOnInit(): void {
+    this.authService.isAuthenticated$().subscribe(() => {
+      this.transformationService.fetchAllTransformations();
+    });
 
-        combineLatest([this.filterChanges, this.searchFilterChanges, this.deprecatedFilterChanges])
-            .pipe(
-                switchMap(([transformationType, searchString, showDeprecated]) =>
-                    this.transformationStore.select(
-                        selectTransformationsByCategoryAndName(
-                            transformationType,
-                            searchString,
-                            showDeprecated
-                        )
-                    )
-                )
+    combineLatest([
+      this.filterChanges,
+      this.searchFilterChanges,
+      this.deprecatedFilterChanges
+    ])
+      .pipe(
+        switchMap(([transformationType, searchString, showDeprecated]) =>
+          this.transformationStore.select(
+            selectTransformationsByCategoryAndName(
+              transformationType,
+              searchString,
+              showDeprecated
             )
-            .subscribe(filteredTransformations => {
-                this.transformationsByCategory = filteredTransformations;
-            });
+          )
+        )
+      )
+      .subscribe(filteredTransformations => {
+        this.transformationsByCategory = filteredTransformations;
+      });
 
-        this.filterChanges.subscribe(() => this.popoverService.closePopover());
-        this.typeFilter.updateValueAndValidity({ emitEvent: true });
-        this.searchFilter.updateValueAndValidity({ emitEvent: true });
-        this.showDeprecatedFilter.updateValueAndValidity({ emitEvent: true });
-    }
+    this.filterChanges.subscribe(() => this.popoverService.closePopover());
+    this.typeFilter.updateValueAndValidity({ emitEvent: true });
+    this.searchFilter.updateValueAndValidity({ emitEvent: true });
+    this.showDeprecatedFilter.updateValueAndValidity({ emitEvent: true });
+  }
 
-    newWorkflow(): void {
-        this.transformationActionService.newWorkflow();
-    }
+  newWorkflow(): void {
+    this.transformationActionService.newWorkflow();
+  }
 
-    newComponent(): void {
-        this.transformationActionService.newComponent();
-    }
+  newComponent(): void {
+    this.transformationActionService.newComponent();
+  }
 
-    closePopover(): void {
-        this.popoverService.closePopover();
-    }
+  closePopover(): void {
+    this.popoverService.closePopover();
+  }
 
-    sortByCategoryAlphabetically(
-        categoryA: KeyValue<string, Transformation[]>,
-        categoryB: KeyValue<string, Transformation[]>
-    ): number {
-        return Utils.string.compare(categoryA.key, categoryB.key);
-    }
+  sortByCategoryAlphabetically(
+    categoryA: KeyValue<string, Transformation[]>,
+    categoryB: KeyValue<string, Transformation[]>
+  ): number {
+    return Utils.string.compare(categoryA.key, categoryB.key);
+  }
 }
--- frontend/src/app/store/transformation/transformation.selectors.ts
+++ frontend/src/app/store/transformation/transformation.selectors.ts
@@ -4,47 +4,47 @@ import { Transformation } from 'src/app/model/transformation';
 import { Utils } from 'src/app/utils/utils';
 import { RevisionState } from '../../enums/revision-state';
 import {
-    transformationEntityAdapter,
-    TransformationState
+  transformationEntityAdapter,
+  TransformationState
 } from './transformation.state';
 
 const { selectAll, selectEntities } =
-    transformationEntityAdapter.getSelectors();
+  transformationEntityAdapter.getSelectors();
 
 export const selectTransformationState =
-    createFeatureSelector<TransformationState>('transformations');
+  createFeatureSelector<TransformationState>('transformations');
 
 export const selectAllTransformations = createSelector(
-    selectTransformationState,
-    (state: TransformationState) =>
-        selectAll(state).filter(
-            transformationRevision =>
-                transformationRevision.state !== RevisionState.DISABLED || true
-        )
+  selectTransformationState,
+  (state: TransformationState) =>
+    selectAll(state).filter(
+      transformationRevision =>
+        transformationRevision.state !== RevisionState.DISABLED || true
+    )
 );
 
 export const selectHashedTransformationLookupById = createSelector(
-    selectAllTransformations,
-    (transformations): Record<string, Transformation> =>
-        transformations.reduce(
-            (acc, transformation): Record<string, Transformation> => ({
-                ...acc,
-                [transformation.id]: transformation
-            }),
-            {}
-        )
+  selectAllTransformations,
+  (transformations): Record<string, Transformation> =>
+    transformations.reduce(
+      (acc, transformation): Record<string, Transformation> => ({
+        ...acc,
+        [transformation.id]: transformation
+      }),
+      {}
+    )
 );
 
 export const selectTransformationById = (transformationId: string) =>
-    createSelector(
-        selectTransformationState,
-        (state: TransformationState) => selectEntities(state)[transformationId]
-    );
+  createSelector(
+    selectTransformationState,
+    (state: TransformationState) => selectEntities(state)[transformationId]
+  );
 
 function filterByName(transformation: Transformation, name: string) {
-    return Utils.string.isEmptyOrUndefined(name)
-        ? true
-        : transformation.name.toLowerCase().includes(name.toLowerCase());
+  return Utils.string.isEmptyOrUndefined(name)
+    ? true
+    : transformation.name.toLowerCase().includes(name.toLowerCase());
 }
 
 /**
@@ -52,29 +52,30 @@ function filterByName(transformation: Transformation, name: string) {
  * Returns a key value object with the categories as keys and the corresponding transformations as values.
  */
 export const selectTransformationsByCategoryAndName = (
-    transformationType: TransformationType,
-    name?: string,
-    includeDeprecated: boolean = false
+  transformationType: TransformationType,
+  name?: string,
+  includeDeprecated: boolean = false
 ) => {
-    return createSelector(
-        selectTransformationState,
-        (state: TransformationState) => {
-            return Object.values(state.entities)
-                .filter(transformation => transformation.type === transformationType)
-                .filter(
-                    transformation => (transformation.state !== RevisionState.DISABLED) || includeDeprecated
-                )
-                .filter(transformation => filterByName(transformation, name))
-                .reduce(
-                    (acc, transformation) => {
-                        if (Utils.isNullOrUndefined(acc[transformation.category])) {
-                            acc[transformation.category] = [];
-                        }
-                        acc[transformation.category].push(transformation);
-                        return acc;
-                    },
-                    {} as { [category: string]: Transformation[] }
-                );
-        }
-    );
+  return createSelector(
+    selectTransformationState,
+    (state: TransformationState) => {
+      return Object.values(state.entities)
+        .filter(transformation => transformation.type === transformationType)
+        .filter(
+          transformation =>
+            transformation.state !== RevisionState.DISABLED || includeDeprecated
+        )
+        .filter(transformation => filterByName(transformation, name))
+        .reduce(
+          (acc, transformation) => {
+            if (Utils.isNullOrUndefined(acc[transformation.category])) {
+              acc[transformation.category] = [];
+            }
+            acc[transformation.category].push(transformation);
+            return acc;
+          },
+          {} as { [category: string]: Transformation[] }
+        );
+    }
+  );
 };
