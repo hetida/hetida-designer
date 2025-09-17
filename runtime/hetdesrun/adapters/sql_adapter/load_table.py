e8b4164ed6a0722518739c5b39c137730f22a2a7
Fabian Wetjen <fwetjen@fuseki.com>
2025-09-17 14:38:44 +0200

    Add docstring to mtsf prep in load_table

--- runtime/hetdesrun/adapters/sql_adapter/load_table.py
+++ runtime/hetdesrun/adapters/sql_adapter/load_table.py
@@ -105,6 +105,19 @@ def prepare_validate_loaded_raw_multitsframe(
     from_datetime: datetime.datetime,
     to_datetime: datetime.datetime,
 ) -> pd.DataFrame:
+    """Prepares and validates a multitsframe.
+
+    Preparation is done by enforcing UTC and correct column naming.
+    For validation, the corresponding function is called.
+
+    Args:
+        metrics_list (list[str] | None):
+            Is only None if 'ALL' was provided in the metrics filter when calling the adapter.
+
+    Returns:
+        pd.DataFrame: A validated multitsframe
+    """
+
     # Guarantee that we have utc timezoned timetsamp column (naive timestamps from db
     # will be assumed to be UTC, non-naive will be transformed into explicit UTC):
     multits_frame[ts_table_config.timestamp_col_name] = pd.to_datetime(
