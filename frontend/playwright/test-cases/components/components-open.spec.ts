ee869545378924742ab8c5bff71809bf6db52702
stewit <you@example.com>
2026-04-24 22:34:46 +0200

    fix e2e test tab clicks

--- frontend/playwright/test-cases/components/components-open.spec.ts
+++ frontend/playwright/test-cases/components/components-open.spec.ts
@@ -1,38 +1,38 @@
 import { expect, test } from '../fixtures/fixture';
 
 test('Open component on double-click', async ({ page, hetidaDesigner }) => {
-  // Arrange
-  const categoryName = 'Arithmetic';
-  const componentName = 'Pi';
-  const componentTag = '1.0.0';
+    // Arrange
+    const categoryName = 'Arithmetic';
+    const componentName = 'Pi';
+    const componentTag = '1.0.0';
 
-  // Act
-  await hetidaDesigner.clickComponentsInNavigation();
-  await hetidaDesigner.clickCategoryInNavigation(categoryName);
+    // Act
+    await hetidaDesigner.clickComponentsInNavigation();
+    await hetidaDesigner.clickCategoryInNavigation(categoryName);
 
-  await hetidaDesigner.doubleClickItemInNavigation(
-    `${componentName}(${componentTag})`
-  );
-  await page.waitForSelector('hd-component-editor');
+    await hetidaDesigner.doubleClickItemInNavigation(
+        `${componentName}(${componentTag})`
+    );
+    await page.waitForSelector('hd-component-editor');
 
-  // Assert
-  // Check for equal names in list and opened tab
-  const componentListName = await page
-    .locator(`mat-expansion-panel:has-text("${categoryName}") >> nth=0`)
-    .locator(`.navigation-item:has-text("${componentName}") >> nth=0`)
-    .locator('.text-ellipsis')
-    .innerText();
-  const componentTabName = await page
-    .locator('div[role="tab"] >> nth=1')
-    .locator('.text-ellipsis')
-    .innerText();
-  expect(componentListName).toEqual(componentTabName);
+    // Assert
+    // Check for equal names in list and opened tab
+    const componentListName = await page
+        .locator(`mat-expansion-panel:has-text("${categoryName}") >> nth=0`)
+        .locator(`.navigation-item:has-text("${componentName}") >> nth=0`)
+        .locator('.text-ellipsis')
+        .innerText();
+    const componentTabName = await page
+        .locator('div[role="tab"] >> nth=2')
+        .locator('.text-ellipsis')
+        .innerText();
+    expect(componentListName).toEqual(componentTabName);
 
-  // Check if hd-component-editor exists
-  const countComponentEditor = await page
-    .locator('hd-component-editor')
-    .count();
-  expect(countComponentEditor).toEqual(1);
+    // Check if hd-component-editor exists
+    const countComponentEditor = await page
+        .locator('hd-component-editor')
+        .count();
+    expect(countComponentEditor).toEqual(1);
 
-  await hetidaDesigner.clearTest();
+    await hetidaDesigner.clearTest();
 });
--- frontend/playwright/test-cases/workflows/workflows-execute-dialog-open.spec.ts
+++ frontend/playwright/test-cases/workflows/workflows-execute-dialog-open.spec.ts
@@ -1,34 +1,34 @@
 import { expect, test } from '../fixtures/fixture';
 
 test('Open "execute workflow" dialog', async ({ page, hetidaDesigner }) => {
-  // Arrange
-  const categoryName = 'Examples';
-  const workflowName = 'Volatility Detection Example';
-  const workflowTag = '1.0.0';
+    // Arrange
+    const categoryName = 'Examples';
+    const workflowName = 'Volatility Detection Example';
+    const workflowTag = '1.0.0';
 
-  // Act
-  await hetidaDesigner.clickWorkflowsInNavigation();
-  await hetidaDesigner.clickCategoryInNavigation(categoryName);
-  await hetidaDesigner.doubleClickItemInNavigation(
-    `${workflowName}(${workflowTag})`
-  );
+    // Act
+    await hetidaDesigner.clickWorkflowsInNavigation();
+    await hetidaDesigner.clickCategoryInNavigation(categoryName);
+    await hetidaDesigner.doubleClickItemInNavigation(
+        `${workflowName}(${workflowTag})`
+    );
 
-  await hetidaDesigner.clickIconInToolbar('Execute');
-  await page.waitForSelector('mat-dialog-container');
+    await hetidaDesigner.clickIconInToolbar('Execute');
+    await page.waitForSelector('mat-dialog-container');
 
-  // Assert
-  const countDialogContainer = await page
-    .locator('mat-dialog-container')
-    .count();
-  expect(countDialogContainer).toEqual(1);
+    // Assert
+    const countDialogContainer = await page
+        .locator('mat-dialog-container')
+        .count();
+    expect(countDialogContainer).toEqual(1);
 
-  // Check for equal substrings in dialog-title and opened tab
-  const dialogTitle = page.locator('.mat-mdc-dialog-title h4');
-  const workflowTabName = await page
-    .locator('div[role="tab"] >> nth=1')
-    .locator('.text-ellipsis')
-    .innerText();
-  await expect(dialogTitle).toContainText(`${workflowTabName}`);
+    // Check for equal substrings in dialog-title and opened tab
+    const dialogTitle = page.locator('.mat-mdc-dialog-title h4');
+    const workflowTabName = await page
+        .locator('div[role="tab"] >> nth=2')
+        .locator('.text-ellipsis')
+        .innerText();
+    await expect(dialogTitle).toContainText(`${workflowTabName}`);
 
-  await hetidaDesigner.clearTest();
+    await hetidaDesigner.clearTest();
 });
